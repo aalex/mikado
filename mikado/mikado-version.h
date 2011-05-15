8c77d733c1c593698904001286b37f5343e52818
Alexandre Quessy <alexandre@quessy.net>
2011-05-14 22:06:23 -0400

    initial import

---
+++ mikado/mikado-version.h
@@ -0,0 +1,20 @@
+#ifndef __MIKADO_VERSION_H__
+#define __MIKADO_VERSION_H__
+
+int mikado_version_major()
+{
+    return @MIKADO_MAJOR_VERSION@;
+}
+
+int mikado_version_minor()
+{
+    return @MIKADO_MAJOR_VERSION@;
+}
+
+int mikado_version_micro()
+{
+    return @MIKADO_MAJOR_VERSION@;
+}
+
+#endif // __MIKADO_VERSION_H__
+
